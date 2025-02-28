3c16ac6c013e98761ea9e764262a8b4f6553d3d1
marcelo.ferreira@tre-am.jus.br <marcelo.ferreira@tre-am.jus.br>
2025-02-28 13:45:09 -0400

    Alteracoes de melhria de navegabilidade

--- ong/formulario-6.py
+++ ong/formulario-6.py
@@ -44,26 +44,33 @@ class MinhaJanela(QMainWindow):
         # Adicionar ações ao menu
         menu_sobre.addAction(acao_mostrar)
 
-        self.botao_ok = QPushButton("OK", self.central_widget)
-        self.botao_ok.move(200, 280)
-        self.botao_ok.setFixedSize(80, 30)
-        self.botao_ok.setVisible(False)
-        self.botao_ok.setStyleSheet('background-color:blue;color:white')
-        self.botao_ok.clicked.connect(self.ocultar_mensagem_versao)
-
         self.botao1 = QPushButton("Gravar", self.central_widget)
         self.botao1.setFixedSize(80, 30)
         self.botao1.move(400,400)
         self.botao1.setVisible(False)
         self.botao1.setStyleSheet('background-color:red;color:white')
-        self.botao1.clicked.connect(self.gravarDados)
+        self.botao1.clicked.connect(self.inserir_registro)
 
         self.botao2 = QPushButton("Voltar", self.central_widget)
         self.botao2.setFixedSize(80,30)
         self.botao2.move(10,400)
         self.botao2.setVisible(False)
         self.botao2.setStyleSheet('background-color:green;color:white')
-        self.botao2.clicked.connect(self.ocultar)
+        self.botao2.clicked.connect(self.ocultar_itens)
+
+        self.botao3 = QPushButton("Atualizar", self.central_widget)
+        self.botao3.setFixedSize(80, 30)
+        self.botao3.move(400,400)
+        self.botao3.setStyleSheet('background-color:red;color:white')
+        self.botao3.setVisible(True)
+        self.botao3.clicked.connect(self.modificarDados)
+
+        self.botao4 = QPushButton("Voltar Consulta", self.central_widget)
+        self.botao4.setFixedSize(120,30)
+        self.botao4.move(10,400)
+        self.botao4.setVisible(False)
+        self.botao4.setStyleSheet('background-color:green;color:white')
+        self.botao4.clicked.connect(self.consultar)
 
         self.lnome = QLabel("Nome:", self.central_widget)
         self.lnome.move(10,10)
@@ -132,47 +139,48 @@ class MinhaJanela(QMainWindow):
         self.tabela.setStyleSheet("background-color: lightblue; color: black")
         # Criar Botão para Alternar Visibilidade
         self.botao_toggle = QPushButton("Voltar")
-        self.botao_toggle.clicked.connect(self.ocultar)
+        self.botao_toggle.clicked.connect(self.ocultar_itens)
         self.botao_toggle.setStyleSheet("background-color: green; color: white;")
         self.botao_toggle.setFixedSize(80, 30)
         layout.addWidget(self.botao_toggle)
         # self.tabela.setStyleSheet("QTableWidget { background-color: lightblue; color: black; }")  # Define a cor do texto para azul
-        self.ocultar()
+        self.ocultar_itens()
 
     def carregar_dados(self):
-        vsql = "SELECT T_NOME, T_RG, T_CPF FROM tb_pessoa order by T_NOME"
+        vsql = "SELECT N_ID, T_NOME, T_CPF FROM tb_pessoa order by T_NOME"
         dados = banco.consultar(vsql)
         self.tabela.setRowCount(len(dados))
         self.tabela.setColumnCount(5)  # ID, Nome, Idade, Ações
-        self.tabela.setColumnWidth(0, 400)   # Coluna 0 com 50 pixels
-        self.tabela.setColumnWidth(1, 100)  # Coluna 1 com 150 pixels
+        self.tabela.setColumnWidth(0, 50)   # Coluna 0 com 50 pixels
+        self.tabela.setColumnWidth(1, 400)  # Coluna 1 com 150 pixels
         self.tabela.setColumnWidth(2, 100)  # Coluna 2 com 100 pixels
         self.tabela.setColumnWidth(3, 16)  # Coluna 2 com 100 pixels
         self.tabela.setColumnWidth(4, 16)  # Coluna 2 com 100 pixels
-        self.tabela.setHorizontalHeaderLabels(["Nome", "RG", "CPF", "", ""])
+        self.tabela.setHorizontalHeaderLabels(["ID", "Nome", "CPF", "", ""])
         for linha_idx, linha in enumerate(dados):
             for col_idx, valor in enumerate(linha):
                 self.tabela.setItem(linha_idx, col_idx, QTableWidgetItem(str(valor)))
+
             # Botão Editar
             botao_editar = QPushButton("")
             icone_editar = QIcon("edicao.png")  # Substitua com o caminho do seu ícone
             botao_editar.setIcon(icone_editar)
             botao_editar.setIconSize(QSize(32, 32))  # Tamanho do ícone
-            botao_editar.setStyleSheet("background-color: green; color black;")
-            botao_editar.clicked.connect(lambda _, row=linha[2]: self.editar_registro(row))
+            # botao_editar.setStyleSheet("background-color: green; color black;")
+            botao_editar.clicked.connect(lambda _, row=linha[0]: self.editar_registro(row))
             self.tabela.setCellWidget(linha_idx, 3, botao_editar)
             # Botão Excluir
             botao_excluir = QPushButton("")
             icone_excluir = QIcon("trash-can-red.jpg")  # Substitua com o caminho do seu ícone
             botao_excluir.setIcon(icone_excluir)
             botao_excluir.setIconSize(QSize(32, 32))  # Tamanho do ícone
-            botao_excluir.setStyleSheet("background-color: red; color white;")
-            botao_excluir.clicked.connect(lambda _, row=linha[2]: self.excluir_registro(row))
+            #botao_excluir.setStyleSheet("background-color: red; color white;")
+            botao_excluir.clicked.connect(lambda _, row=linha[0]: self.excluir_registro(row))
             self.tabela.setCellWidget(linha_idx, 4, botao_excluir)
 
     def editar_registro(self, id_registro):
         # QMessageBox.information(self, "Editar", f"Editar formulário de {id_registro}")
-        vsql = "SELECT * FROM tb_pessoa WHERE T_CPF = "+ str(id_registro)
+        vsql = "SELECT * FROM tb_pessoa WHERE N_ID = "+ str(id_registro)
         resultado = banco.consultar(vsql)
         self.lenome.setText(resultado[0][1])
         self.lerg.setText(resultado[0][2])
@@ -181,30 +189,32 @@ class MinhaJanela(QMainWindow):
         self.lecpfmae.setText(resultado[0][5])
         self.lepai.setText(resultado[0][6])
         self.lecpfpai.setText(resultado[0][7])
-        self.inserir()
-        self.botao1.setVisible(False)
-        self.botao2.setVisible(False)
-        self.botao3 = QPushButton("Modificar", self.central_widget)
-        self.botao3.setFixedSize(80, 30)
-        self.botao3.move(400,400)
-        self.botao3.setStyleSheet('background-color:red;color:white')
-        self.botao3.setVisible(True)
-        self.botao3.clicked.connect(self.gravarDados)
-        sell.consultar()
-        self.botao3.setVisible(False)
+        self.modificar()
 
     def excluir_registro(self, id_registro):
         """Exclui um registro do banco de dados"""
         resposta = QMessageBox.question(self, "Excluir", f"Tem certeza que deseja excluir o {id_registro}?",
                                         QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
         if resposta == QMessageBox.StandardButton.Yes:
-            vsql = "DELETE FROM tb_pessoa WHERE T_CPF = "+ str(id_registro)
+            vsql = "DELETE FROM tb_pessoa WHERE N_ID = "+ str(id_registro)
             print(vsql)
             banco.atualizar(vsql)
             self.carregar_dados()  # Recarregar os dados após a exclusão
 
     def inserir(self):
-        self.ocultar()
+        self.ocultar_itens()
+        self.limpar_dados()
+        self.mostrar_formulario()
+
+    def modificar(self):
+        self.ocultar_itens()
+        self.mostrar_formulario()
+        self.botao1.setVisible(False)
+        self.botao2.setVisible(False)
+        self.botao3.setVisible(True)
+        self.botao4.setVisible(True)
+
+    def mostrar_formulario(self):
         self.botao1.setVisible(True)
         self.botao2.setVisible(True)
         self.lnome.setVisible(True)
@@ -221,18 +231,19 @@ class MinhaJanela(QMainWindow):
         self.lecpfmae.setVisible(True)
         self.lepai.setVisible(True)
         self.lecpfpai.setVisible(True)
-        # self.linhacpf.setVisible(True)
 
     def consultar(self):
         self.carregar_dados()
         self.tabela.setVisible(True)
         self.botao_toggle.setVisible(True)
 
-    def ocultar(self):
+    def ocultar_itens(self):
         self.botao_toggle.setVisible(False)
         self.tabela.setVisible(False)
         self.botao1.setVisible(False)
         self.botao2.setVisible(False)
+        self.botao3.setVisible(False)
+        self.botao4.setVisible(False)
         self.lnome.setVisible(False)
         self.lenome.setVisible(False)
         self.lrg.setVisible(False)
@@ -248,7 +259,7 @@ class MinhaJanela(QMainWindow):
         self.lepai.setVisible(False)
         self.lecpfpai.setVisible(False)
 
-    def gravarDados(self):
+    def inserir_registro(self):
         vnome=self.lenome.text()
         vrg=self.lerg.text()
         vcpf=self.lecpf.text()
@@ -259,6 +270,9 @@ class MinhaJanela(QMainWindow):
         #vsql= "INSERT INTO tb_pessoa (T_NOME, [N_RG], [N_CPF], T_MAE, [N_CPF-MAE], T_PAI, [N_CPF-PAI]) VALUES('"+vnome+"',(vrg),(vcpf),'"+vmae+"',(vcpfmae),'"+vpai+"',(vcpfpai)"
         vsql="INSERT INTO tb_pessoa (T_NOME, T_RG, T_CPF, T_MAE, T_CPFMAE, T_PAI, T_CPFPAI) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s')" %(vnome, vrg, vcpf, vmae, vcpfmae, vpai, vcpfpai)
         banco.atualizar(vsql)
+        self.limpar_dados()
+
+    def limpar_dados(self):
         self.lenome.clear()
         self.lerg.clear()
         self.lecpf.clear()
@@ -267,13 +281,9 @@ class MinhaJanela(QMainWindow):
         self.lepai.clear()
         self.lecpfpai.clear()
 
-    def mostrar(self):
-        self.label_versao.setVisible(True)
-        self.botao_ok.setVisible(True)
-
-    def ocultar_mensagem_versao(self):
-        self.label_versao.setVisible(False)
-        self.botao_ok.setVisible(False)
+    def modificarDados(self):
+        self.inserir_registro()
+        self.consultar()
 
     def mostrar_mensagem_sobre(self):
         mensagem = QMessageBox(self)
